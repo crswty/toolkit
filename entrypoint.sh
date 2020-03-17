2b3273d72d98c3abe96896288a29bbbecf0f4d3a
Chris Whitty <1538281+crswty@users.noreply.github.com>
2020-03-17 23:57:43 +0000

    Add a basic help command

--- entrypoint.sh
+++ entrypoint.sh
@@ -2,12 +2,25 @@
 
 COMMAND=${1:-run}
 
+help() {
+  cat <<EOF
+Commands:
+  run - Run the container starting a new bash session
+  wait - Start the container making it run forever
+  k8s - Print out a Kubernetes Pod resource that can be used to create a running toolkit in k8s
+  help - show the help
+EOF
+}
+
 run() {
   if [ -t 1 ]
   then
     /bin/bash
   else
-    echo "Run command requires an interactive tty. If running with Docker/Kubernetes add the -it flag"
+    echo "ERR: Run command requires an interactive tty. If running with Docker/Kubernetes add the -it flag"
+    echo
+    help
+    exit 1
   fi
 }
 
@@ -24,10 +37,11 @@ waitforever() {
 }
 
 case "$COMMAND" in
-  wait)  waitforever ;;
+  wait) waitforever ;;
   run)  run ;;
   k8s)  k8s ;;
-  *) echo "Unknown command $COMMAND"
+  help) help ;;
+  *) echo "ERR: Unknown command $COMMAND"; echo; help; exit 1
 esac
 
 
